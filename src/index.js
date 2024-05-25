5fc8db432e226edfab80b99912118d4da0b64006
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-05-25 12:13:32 -0400

    Webpack Installed
    Installed webpack and set everything up.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const element = document.createElement('div');
+    element.classList.add('hello');
+    element.textContent = "Hello"
+ 
+
+ document.body.appendChild(element); 
